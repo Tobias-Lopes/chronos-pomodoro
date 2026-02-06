3862ccc47da9015ab13c04b8ab2a039efd786c1c
Tobias Lopes <tobias.lopes.712@ufrn.edu.br>
2026-02-05 21:21:04 -0300

    Criando meu primeiro componente do React com JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// PascalCase
+// App
 
-function App() {
-  const [count, setCount] = useState(0);
+export function App() {
+  console.log('Oi');
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <h1>Olá mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quae neque
+        minima? Blanditiis, non ea, atque est vero quia eos, asperiores maxime
+        maiores repellendus nemo eum laboriosam modi odit obcaecati.
       </p>
     </>
   );
 }
-
-export default App;
