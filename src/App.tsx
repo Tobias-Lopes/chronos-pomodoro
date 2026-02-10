7167819b359a01a86411ffbde310df8761c0aa15
Tobias Lopes <tobias.lopes.712@ufrn.edu.br>
2026-02-10 07:15:23 -0300

    Criando a estrutura HTML e CSS do nosso app para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 // PascalCase
 // App
 
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola Mundo!!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quae neque
-        minima? Blanditiis, non ea, atque est vero quia eos, asperiores maxime
-        maiores repellendus nemo eum laboriosam modi odit obcaecati.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
     </>
   );
 }
