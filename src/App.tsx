fa137fe7e79bb7aba623c9d5e335753cdec11810
Tobias Lopes <tobias.lopes.712@ufrn.edu.br>
2026-02-09 22:10:02 -0300

    Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Ola Mundo!!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quae neque
         minima? Blanditiis, non ea, atque est vero quia eos, asperiores maxime
