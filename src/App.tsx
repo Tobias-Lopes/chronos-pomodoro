65cb507171391a656a8a24f0a3ea693a2787b0f0
Tobias Lopes <tobias.lopes.712@ufrn.edu.br>
2026-02-13 08:26:38 -0300

    feat: Criando componente de menu com links e ícones

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
@@ -15,7 +15,7 @@ export function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
