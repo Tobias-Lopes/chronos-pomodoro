39143789de9b63bd95baf6aa92bcc8c510ba5658
Tobias Lopes <tobias.lopes.712@ufrn.edu.br>
2026-05-03 16:31:04 -0300

    Componente GenericHtml para páginas que renderizam HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Home } from './pages/Home/Index';
+import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
-
 
 export function App() {
   return <Home />;
---
+++ src/components/GenericHtml/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+interface GenericHtmlProps {
+  children: React.ReactNode;
+}
+
+export function GenericHtml({children}: GenericHtmlProps) {
+  return <div className={styles.genericHtml}>
+    {children}
+  </div>
+}
